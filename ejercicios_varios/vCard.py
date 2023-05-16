202d63e92683e281b89e3c80c7c26d14a3316839
Jose Constantin <74998233+jacq2101@users.noreply.github.com>
2023-05-15 22:54:17 -0600

    Update vCard.py

--- ejercicios_varios/vCard.py
+++ ejercicios_varios/vCard.py
@@ -1,15 +1,22 @@
 import csv
 
-with open('datos.csv') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
+# función para generar el nombre del archivo
+def get_file_name(index):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    letter_index = (index // 26) % 26
+    number_index = index % 26 + 1
+    file_name = f"{letters[letter_index]}{number_index}"
+    return file_name
+
+with open('datos.csv') as f:
+    reader = csv.DictReader(f)
+    for index, row in enumerate(reader):
         first_name = row['\ufeffNombre']
         last_name = row['Apellido']
         phone = row['Celular']
         email = row['Email']
         work_title = row['Puesto de trabajo']
         office_address = f"{row['Calle Oficina']}, {row['Colonia']}, {row['Ciudad']}, {row['Estado']}, {row['Codigo Postal']}, {row['País']}"
-
-        vcf_file_name = f"{first_name}_{last_name}.vcf"
+        vcf_file_name = f"{get_file_name(index)}.vcf"
         with open(vcf_file_name, 'w') as vcf_file:
             vcf_file.write(f"BEGIN:VCARD\nVERSION:3.0\nN:{last_name};{first_name};;;\nFN:{first_name} {last_name}\nORG:\nTEL;TYPE=CELL:{phone}\nEMAIL;TYPE=PREF,INTERNET:{email}\nTITLE:{work_title}\nADR;TYPE=WORK:;;{office_address}\nEND:VCARD\n")
